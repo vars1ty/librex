be5b2dbd4b3baa7dc6ba8cd36c51e2c792e213f3
vars1ty <narcotics@startmail.com>
2022-11-08 15:56:37 +0100

    Make footer smaller

--- misc/footer.php
+++ misc/footer.php
@@ -1,8 +1,10 @@
-<div class="footer-container">
-    <a href="./">LinXer</a>
-    <a href="https://github.com/vars1ty/librex/" target="_blank">Source</a>
-    <a href="./faq.php">F.A.Q</a>
-    <a href="./settings.php">Settings</a>
-    <a href="./api.php" target="_blank">API</a>
-    <a href="./donate.php">Donate ❤️</a>
-</div>
+<?php
+// Same story as with header.
+echo "<div class='footer-container'>";
+echo "<a href='./'>LinXer</a>";
+echo "<a href='https://github.com/vars1ty/librex/' target='_blank'>Source</a>";
+echo "<a href='./faq.php'>F.A.Q</a>";
+echo "<a href='./settings.php'>Settings</a>";
+echo "<a href='./api.php' target='_blank'>API</a>";
+echo "<a href='./donate.php'>Donate ❤️</a>";
+echo "</div>";
