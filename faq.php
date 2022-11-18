6a788e396cdf3300079bfd1fbf1ad32ca1d7100e
vars1ty <narcotics@startmail.com>
2022-11-18 17:50:17 +0100

    Enable CloudFlare temporarily

--- faq.php
+++ faq.php
@@ -22,8 +22,7 @@ function print_qa($question, $answer): void
     <br>The IP is not tied to anything like your search queries, it's just a line like this: <code>1.1.1.1</code>.");
     print_qa("Why Romania?", "Because Romania has solid legislation, has GDPR and takes privacy seriously.");
     print_qa("Why FlokiNET as your host?", "FlokiNET was chosen because it's cheap, ignores DMCA requests, respects user-privacy and is hosted in<br>🇮🇸 Iceland.");
-    print_qa("Is CloudFlare being used?", "No, CloudFlare is not currently being used. As to why that is, is because it limits the amount of companies that can access <b>your</b> data.
-<br>Instead, LinXer is relying on FlokiNET for DDoS Protection.");
+    print_qa("Is CloudFlare being used?", "Yes, CloudFlare is being used temporarily to help mitigate potential load issues.");
     print_qa("Where are you getting your search results from?", "Google, Qwant and Brave. Then there's other sources for torrents, special query lookups, etc.
     <br>You may find more information on the projects GitHub-page.");
     print_qa("How can you hide my IP from the search engines you are scraping?", "By using CURL, the engines <b>only</b> get the IP-Address of the website which is in Romania, then it's displayed for you.
